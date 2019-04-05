38043a88b51ca68f8cfcc27771fe30fe5898a772
Daisuke Hirata <daisukihirata@gmail.com>
2019-04-05 10:56:10 +0900

    first commit

---
+++ test/application.spec.ts
@@ -0,0 +1,19 @@
+import chaiHttp = require("chai-http");
+import * as chai from "chai";
+import app from "../src/application";
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe("App", () => {
+  it("works", (done: () => void): void => {
+  chai.request(app)
+      .get("/docs/")
+      .send({})
+      .end((err: Error, res: any): void => {
+          expect(res.statusCode).to.be.equal(200);
+          done();
+      });
+
+    });
+});
