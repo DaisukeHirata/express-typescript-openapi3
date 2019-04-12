f6a6f41b16f5ded41ce7729cd65bf849b7d428eb
Daisuke Hirata <daisukihirata@gmail.com>
2019-04-12 21:53:17 +0900

    add json-serializers

--- src/controllers/movies.ts
+++ src/controllers/movies.ts
@@ -1,32 +1,39 @@
 import { Request, Response } from "express";
 import * as P from "bluebird";
 import { TDebug } from "../log";
+import { movieSerializer } from "../serializers/movies";
 
 const debug = new TDebug("app:src:controllers:movies");
 
-const movie = {
-  "type": "movies",
-  "id": 1,
-  "attributes": {
-    "title": "Assasins Creed",
-    "runtime": 115,
-    "format": "IMAX",
-    "plot": "Lorem ipsum dolor sit amet",
-    "released_at": "2018-12-01T00:00:00Z"
-  }
+const movie1 = {
+  id: 1,
+  title: "Assasins Creed",
+  runtime: 115,
+  format: "IMAX",
+  plot: "Lorem ipsum dolor sit amet",
+  released_at: "2018-12-01T00:00:00Z"
+};
+
+const movie2 = {
+  id: 2,
+  title: "Assasins Creed 2",
+  runtime: 116,
+  format: "IMAX MAX",
+  plot: "Lorem ipsum dolor sit amet",
+  released_at: "2018-12-01T00:00:00Z"
 };
 
 export async function getMovies(req: Request, res: Response): P<any> {
-  const movies = {
-    "data": [movie]
-  };
+  const movies = [movie1, movie2];
   debug.log("movies: ", movies);
-  res.send(movies);
+  const serializedMovies = movieSerializer.serialize(movies);
+  res.send(serializedMovies);
 }
 
 export async function getMovie(req: Request, res: Response): P<any> {
   const id = req.swagger.params.id.value;
-  movie.id = id;
-  debug.log("movie: ", movie);
-  res.send(movie);
+  movie1.id = id;
+  const serializedMovie = movieSerializer.serialize([movie1]);
+  debug.log("movie: ", serializedMovie);
+  res.send(serializedMovie);
 }
