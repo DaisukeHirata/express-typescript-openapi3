1e25ae74ed8f9483add7eff2e0e2eb18b8d70ff6
Daisuke Hirata <daisukihirata@gmail.com>
2019-04-15 14:29:38 +0900

    remove test data from repo

--- src/repositories/moviesRepository.ts
+++ src/repositories/moviesRepository.ts
@@ -14,24 +14,6 @@ const mysql = serverlessMysql({
   }
 });
 
-const movie1 = {
-  id: "1",
-  title: "Assasins Creed",
-  runtime: 115,
-  format: "IMAX",
-  plot: "Lorem ipsum dolor sit amet",
-  released_at: "2018-12-01T00:00:00Z"
-};
-
-const movie2 = {
-  id: "2",
-  title: "Assasins Creed 2",
-  runtime: 116,
-  format: "IMAX MAX",
-  plot: "Lorem ipsum dolor sit amet",
-  released_at: "2018-12-01T00:00:00Z"
-};
-
 export class MovieRepository {
   public async getAllMovies(): P<any[]> {
     const results = await mysql.query("SELECT id, title, runtime, format, plot, DATE_FORMAT(released_at, \'%Y-%m-%dT%TZ\') as released_at FROM movie");
