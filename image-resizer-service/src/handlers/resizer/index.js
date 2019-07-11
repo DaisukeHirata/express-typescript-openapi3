3c2fe11a19416cef23c2f04e6c89c2ecb4489290
Daisuke Hirata <daisukihirata@gmail.com>
2019-07-12 00:11:40 +0900

    image resizer service

---
+++ image-resizer-service/src/handlers/resizer/index.js
@@ -0,0 +1,18 @@
+import { resizeHandler } from './resizeHandler'
+
+export const handler = async (event) => {
+  try {
+    const imagePath = await resizeHandler._process(event)
+    console.log(imagePath);
+    const URL = `http://${process.env.BUCKET}.s3-website-${process.env.REGION}.amazonaws.com`
+
+    return {
+      headers: { 'location': `${URL}/${imagePath}` },
+      statusCode: 301,
+      body: ''
+    }
+  } catch (error) {
+    console.log(error)
+    return new Error(error)
+  }
+}
