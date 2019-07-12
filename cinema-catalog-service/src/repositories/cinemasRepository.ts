641285ed93749afc191d79c4931bb892fe8ecf65
Daisuke Hirata <daisukihirata@gmail.com>
2019-07-12 23:02:17 +0900

    🔌 fix Circuit breakers

--- cinema-catalog-service/src/repositories/cinemasRepository.ts
+++ cinema-catalog-service/src/repositories/cinemasRepository.ts
@@ -7,6 +7,8 @@ import { injectable } from "inversify";
 import { ICinemaRepository } from "../inversify/interfaces";
 import { fetch, post } from "../lib/circuitBreaker";
 import { cinemaDeserializer } from "../serializers/cinemasSerializer";
+import { TDebug } from "../log";
+const debug = new TDebug("app:src:repositories:cinemas");
 
 // current workaround. https://github.com/jeremydaly/serverless-mysql/issues/30#issuecomment-488192023
 const createConnection = (serverlessMysql as unknown) as (
@@ -15,16 +17,18 @@ const createConnection = (serverlessMysql as unknown) as (
 
 const mysql = createConnection({
   config: {
-    host     : env.get("DATABASE_HOST"),
-    database : env.get("DATABASE"),
-    user     : env.get("DATABASE_USER"),
-    password : env.get("DATABASE_PASSWORD"),
+    host: env.get("DATABASE_HOST"),
+    database: env.get("DATABASE"),
+    user: env.get("DATABASE_USER"),
+    password: env.get("DATABASE_PASSWORD"),
     dateStrings: true
   }
 });
 
 const moviesServiceHost = env.get("MOVIES_SERVICE_HOST");
 const searchServiceHost = env.get("SEARCH_SERVICE_HOST");
+
+const fallbackResponse = { "data": [{ "type": "movies", "id": "1", "attributes": { "title": "Assasins Creed", "genre": "Action", "runtime": 115, "format": "IMAX", "plot": "In 1492 Andalusia, during the Granada War, Aguilar de Nerha is accepted into the Assassins Brotherhood. He is assigned to protect Prince Ahmed de Granada from the Knights Templar. In 1986, adolescent Callum Cal Lynch finds his mother killed by his father, Joseph, a modern-day Assassin. Gunmen led by Alan Rikkin, CEO of the Templars' Abstergo Foundation, arrive to capture Joseph, who convinces his son to escape.", "plot-ja": "主人公・カラムは幼少時に、アサシンの装束をまとった父親に母親を殺されるという悲惨な経験をし、その十数年後に殺人を犯し刑務所に収監、死刑が執行される。死んだはずのカラムが目を覚ましたのは、複合企業・アブスターゴ社がスペインで運営する矯正施設の医務室だった。", "plot-cn": "2016年，死囚柯倫·林區透過阿布斯泰戈（Abstergo）科技公司察覺自己其實是刺客組織的後裔，他被迫參與測驗來進行深層記憶的探索，以便重溫15世紀末1492年祖先阿基拉闖盪西班牙宗教裁判所的經歷。林區在探索的過程中，必須獲得其知識和技能，以面對聖殿騎士團及存於現今的敵人。", "released-at": "2018-12-01T00:00:00Z" } }] };
 
 @injectable()
 export class CinemaRepository implements ICinemaRepository {
@@ -96,22 +100,23 @@ export class CinemaRepository implements ICinemaRepository {
 
     // transform object array to nested object to reduce data redandancy
     const nestedRooms = rooms.reduce((result, room) => {
-      const a = result.find(({id}) => id === room.id);
+      const a = result.find(({ id }) => id === room.id);
       const { time, price } = room;
       if (a) {
-        a.schedules.push({time, price});
+        a.schedules.push({ time, price });
       } else {
         result.push({
           id: room.id,
           name: room.name,
           capacity: room.capacity,
-          schedules: [{time, price}]
+          schedules: [{ time, price }]
         });
       }
       return result;
     }, []);
 
-    const movie = await fetch(moviesServiceHost + "/api/movies/" + movieId).catch((err) => {
+    const movie = await fetch(moviesServiceHost + "/api/movies/" + movieId, fallbackResponse).catch((err) => {
+      debug.error(JSON.stringify(err));
       throw err;
     });
     const deserializedMovie = await cinemaDeserializer.deserialize(movie);
@@ -159,7 +164,7 @@ export class CinemaRepository implements ICinemaRepository {
     });
 
     const cinemasMovies = cinemas.map((cinema) => {
-      const movie = movies.find(({id}) => cinema.movie_id === id);
+      const movie = movies.find(({ id }) => cinema.movie_id === id);
 
       cinema["title"] = movie.title;
       cinema["plot"] = movie.plot;
