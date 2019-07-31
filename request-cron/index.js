3b91837abd372aa105950acb0117d44aab93cae4
Daisuke Hirata <daisukihirata@gmail.com>
2019-07-31 23:21:44 +0900

    request cron log only res body

--- request-cron/index.js
+++ request-cron/index.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 export const hello = async (event, context, callback) => {
   const res = await axios({
     method: "get",
-    url: "https://XXX",
+    url: "https://xxx",
   })
-  console.log(JSON.stringify(res));
+  console.log(JSON.stringify(res.data));
 
   callback(null, {
     message: 'success',
